1117caa3eaa9bd884824eadb67cea900ab11c72a
gabrieltherock <gabrieldarocha2013@hotmail.com>
2022-05-15 12:37:08 -0300

    refatorando reajuste

--- src/main/java/br/com/gabriel/service/ReajusteService.java
+++ src/main/java/br/com/gabriel/service/ReajusteService.java
@@ -8,22 +8,7 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        BigDecimal reajuste;
-
-        switch (desempenho) {
-            case A_DESEJAR:
-                reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-                break;
-            case BOM:
-                reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-                break;
-            case OTIMO:
-                reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-                break;
-            default:
-                return;
-        }
-
+        BigDecimal reajuste = funcionario.getSalario().multiply(desempenho.percentualReajuste());
         funcionario.reajustarSalario(reajuste);
     }
 }
